7a3f77aa8def08bcf687b393acb5aa8949f5f6f1
gosp <141959@qq.com>
2017-07-06 16:45:23 +0800

    cgo done.

---
+++ destroy.go
@@ -0,0 +1,9 @@
+package gmagick
+
+import "github.com/gographics/gmagick/types"
+
+// Destroy instance of Destroyer
+// If GOGC=off you should call obj.Destroy() manually
+func Destroy(d types.Destroyer) {
+	d.Destroy()
+}
